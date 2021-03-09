27779313cfb8b25113a7bd235ac908d288e43f9d
Max <73542409+roswerk@users.noreply.github.com>
2021-03-09 19:07:29 +0100

    Update JS List with Pokemon characteristics

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-alert("Hello World!");
-
-var favoriteFood = "Lassagna";
-
-document.write(favoriteFood); 
+let pokemonList = [{name: "Bulbasaur", height: 7, type: "Grass"},
+{name: "Charmander" , height: 4, type: "Fire" },
+{name: "Pikachu" , height: 2, type: "Electricity" },
+{name: "Oddish" , height: 1, type: "Poison" }
+];
