33964b187044c074f9ceafbf977cf96d6b7d4195
Max <73542409+roswerk@users.noreply.github.com>
2021-03-10 10:28:45 +0100

    Display name of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ let pokemonList = [{name: "Bulbasaur", height: 7, type: "Grass"},
 {name: "Pikachu" , height: 2, type: "Electricity" },
 {name: "Oddish" , height: 1, type: "Poison" }
 ];
+
+document.write("The available pokemons are: ");
+
+document.write(pokemonList[0].name + ",", "\n",
+pokemonList[1].name + ",", "\n",
+pokemonList[2].name + ",", "\n",
+pokemonList[3].name + ".", "\n");
