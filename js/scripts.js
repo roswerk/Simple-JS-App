1e55efad6fdf648043f5c4062db8e8a8c731a6d4
Max <73542409+roswerk@users.noreply.github.com>
2021-03-11 17:54:37 +0100

    Update function to take any parameters

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,36 @@ let pokemonList = [{name: "Charmander" , height: 4, type: "Fire" },
 {name: "Oddish" , height: 1, type: "Poison" }
 ];
 
+let pokemonList2 = [{name: "Onix" , height: 6, type: "Stone" },
+{name: "Dragonair" , height: 2, type: "Water" },
+{name: "Ivysaur", height: 3, type: "Grass"},
+{name: "Butterfree" , height: 1, type: ["Poison", "Flying]"]}
+];
+
+// Workig function
+
+function printArrayDetails(pokemonArray){
 // Print all available pokemons in the DOM
-for (let i=0; i < pokemonList.length; i++){
+for (let i=0; i < pokemonArray.length; i++){
 
 // If a pokemon has height over 5, display a message "Wow thats huge next to it" - Only one pokemon should evaluate true
-if (pokemonList[i].height > 5){
-    document.write(pokemonList[i].name + " " + "(" + "Height " + pokemonList[i].height + ")" + " -Wow that Pokemon is HUGE!" + "<br>" + "<br>");
+if (pokemonArray[i].height > 5){
+    document.write("<p>" + pokemonArray[i].name + " " + "(" + "Height " + pokemonArray[i].height + ")" + " -Wow that Pokemon is HUGE!" + "</p>");
   }
 
   else{
-document.write(pokemonList[i].name + " " + "(" + "Height " + pokemonList[i].height + ")" + "<br>" + "<br>");
+document.write("<p>" + pokemonArray[i].name + " " + "(" + "Height " + pokemonArray[i].height + ")" + "</p>");}
 }
 
-
-
-
 }
+
+printArrayDetails(pokemonList);
+
+
+
+
+
+
 
 
 
